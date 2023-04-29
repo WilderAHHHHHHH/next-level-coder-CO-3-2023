6fe3732412a44ed27b018cd25e2685977d29d30d
WilderAHHHHHHH <Wilder.Tabares@fundacion-jala.org>
2023-04-28 19:15:52 -0500

    class 2: Dinosaur run

--- dino_runner/components/game.py
+++ dino_runner/components/game.py
@@ -1,7 +1,7 @@
 import pygame
 
 from dino_runner.utils.constants import BG, ICON, SCREEN_HEIGHT, SCREEN_WIDTH, TITLE, FPS
-
+from dino_runner.components.dinosaur import Dinosaur
 
 class Game:
     def __init__(self):
@@ -15,6 +15,8 @@ class Game:
         self.x_pos_bg = 0
         self.y_pos_bg = 380
 
+
+        self.player = Dinosaur()
     def run(self):
         # Game loop: events - update - draw
         self.playing = True
@@ -30,12 +32,14 @@ class Game:
                 self.playing = False
 
     def update(self):
-        pass
+        user_imput = pygame.key.get_pressed()
+        self.player.update(user_imput)
 
     def draw(self):
         self.clock.tick(FPS)
         self.screen.fill((255, 255, 255))
         self.draw_background()
+        self.player.draw(self.screen)
         pygame.display.update()
         pygame.display.flip()
 
